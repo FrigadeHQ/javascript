e1ce7aef82c0c9a4f85f1ad0b8b30e7d0b8aa0b8
christianmat <christian_mathiesen@alumni.brown.edu>
2023-02-02 00:43:31 -0800

    fix(cors): allow cors

--- src/api/common.ts
+++ src/api/common.ts
@@ -6,7 +6,7 @@ export const API_PREFIX = 'https://api.frigade.com/v1/public/';
 
 export function useConfig() {
   const {publicApiKey, userId} = React.useContext(FrigadeContext);
-  
+
   return {
     config: useMemo(
       () => ({
@@ -15,6 +15,8 @@ export function useConfig() {
           'X-User-Id': userId ?? 'guest',
           'Content-Type': 'application/json'
         },
+        mode: 'no-cors',
+        crossDomain:true
       }),
       [publicApiKey, userId]
     )
