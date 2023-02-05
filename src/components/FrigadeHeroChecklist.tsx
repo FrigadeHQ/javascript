531e17ce428a326eaf35f4a751608b928382da41
christianmat <christian_mathiesen@alumni.brown.edu>
2023-02-04 23:00:56 -0800

    fix: integrate hero checklist component

---
+++ src/components/FrigadeHeroChecklist.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Checklist, HeroChecklistProps, HeroChecklist } from '@frigade/react-onboarding-components'
+import { useFlows } from '../api/flows'
+
+interface FrigadeHeroChecklistProps {
+  flowId: string
+}
+
+// Create FC with FrigadeHeroChecklistProps & CheckListProps
+export const FrigadeHeroChecklist: React.FC<FrigadeHeroChecklistProps & HeroChecklistProps> = ({
+  flowId,
+  ...props
+}) => {
+  const { getFlow, getFlowSteps, markStepStarted, markStepCompleted, getStepStatus } = useFlows()
+
+  const flow = getFlow(flowId)
+  if (!flow) {
+    return null
+  }
+
+  const steps = getFlowSteps(flowId)
+  if (!steps) {
+    return null
+  }
+
+  return (
+    <HeroChecklist
+      // Override CTA clicks on each step
+      steps={steps.map((step) => {
+        return {
+          ...step,
+          complete: getStepStatus(flowId, step.id),
+          handleCTAClick: () => {
+            markStepCompleted(flowId, step.id)
+          },
+        }
+      })}
+      {...props}
+    />
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { FrigadeProvider } from './FrigadeProvider'
 export { useFlows } from './api/flows'
 export { useFlowResponses } from './api/flow-responses'
 export { useUser } from './api/users'
-
+export { FrigadeHeroChecklist } from './components/FrigadeHeroChecklist'
