0418f622998c0f25ca11f2ff65795632405130c1
christianmat <christian_mathiesen@alumni.brown.edu>
2023-02-07 09:32:41 -0800

    fix: add local storage for guest user id

--- src/DataFetcher/index.tsx
+++ src/DataFetcher/index.tsx
@@ -2,9 +2,11 @@ import React, { FC, useContext, useEffect } from 'react'
 import { useFlows } from '../api/flows'
 import { FrigadeContext } from '../FrigadeProvider'
 import { useUser } from '../api/users'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 interface DataFetcherProps {}
+
+const guestUserIdField = 'xFrigade_guestUserId'
 
 export const DataFetcher: FC<DataFetcherProps> = ({}) => {
   const { getFlows } = useFlows()
@@ -24,7 +26,16 @@ export const DataFetcher: FC<DataFetcherProps> = ({}) => {
   function generateGuestUserId() {
     // If userId is null, generate a guest user id using uuid
     if (userId === null) {
-      setUserId('guest_' + uuidv4())
+      // Call local storage to see if we already have a guest user id
+      const guestUserId = localStorage.getItem(guestUserIdField)
+      if (guestUserId) {
+        setUserId(guestUserId)
+        return
+      }
+      // If we don't have a guest user id, generate one and save it to local storage
+      const newGuestUserId = 'guest_' + uuidv4()
+      localStorage.setItem(guestUserIdField, newGuestUserId)
+      setUserId(newGuestUserId)
     }
   }
 
--- src/api/users.tsx
+++ src/api/users.tsx
@@ -3,6 +3,6 @@ import { FrigadeContext } from '../FrigadeProvider'
 
 export function useUser() {
   const { userId, setUserId } = useContext(FrigadeContext)
-  // TODO: if a user calls setUserId with a new value, call backend link the two user ids
+
   return { userId, setUserId }
 }
