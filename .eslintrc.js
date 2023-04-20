ec2796486ae580876ab9f606895422e49f2a08fd
Denis <someden@ya.ru>
2023-04-20 17:29:07 +0300

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,38 @@
 module.exports = {
-  'parser': '@typescript-eslint/parser',
-  'extends': [
-    'airbnb',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-native/all',
-  ],
-  'plugins': [
-    'prettier',
-    'react'
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
-    }
+  root: true,
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'plugin:react-native/all', 'prettier'],
+  env: {
+    browser: true,
+    jest: true,
   },
-  'rules': {
+  parserOptions: {
+    project: ['./tsconfig-eslint.json'],
+  },
+  rules: {
+    'no-plusplus': 'off',
+    'react/prop-types': 'off',
+    eqeqeq: 'off',
+    'object-shorthand': 'off',
+    'prefer-destructuring': 'off',
+    'react/destructuring-assignment': 'off',
     'react/require-default-props': 'off',
+    'react/no-array-index-key': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-native/no-inline-styles': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/no-cycle': 'off',
+    'import/order': 'off',
     'import/prefer-default-export': 'off',
-    'react-hooks/exhaustive-deps':          'warn',
-    'react-hooks/rules-of-hooks':           'error',
-    'react/jsx-filename-extension':         [1, { 'extensions': ['.ts', '.tsx'] }],
-    'react/jsx-indent-props':               [2, 4],
-    'react/jsx-indent':                     [2, 4],
-    'react/jsx-one-expression-per-line':    [0],
-    'react/prefer-stateless-function':      [1],
-    'react/static-property-placement':      [1, 'property assignment'],
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-      },
-    ],
-  }
-};
+    'no-underscore-dangle': 'off',
+    'arrow-body-style': 'off',
+    'consistent-return': 'off',
+    'prefer-template': 'off',
+    'spaced-comment': 'off',
+    'no-else-return': 'off',
+  },
+}
