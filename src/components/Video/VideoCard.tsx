0eb6aa75389be7a5838a48ff02aa289ae768e972
christianmat <christian_mathiesen@alumni.brown.edu>
2023-05-08 18:52:36 -0700

    fix: support both native and youtube videos

--- src/components/Video/VideoCard.tsx
+++ src/components/Video/VideoCard.tsx
@@ -36,6 +36,13 @@ const PlayIconWrapper = styled.div`
 const VideoSource = styled.video`
   width: 100%;
   height: 100%;
+  min-height: 200px;
+  border-radius: ${(props) => props.appearance.theme.borderRadius}px;
+`
+const YouTubeVideoSource = styled.iframe`
+  width: 100%;
+  height: 100%;
+  min-height: 200px;
   border-radius: ${(props) => props.appearance.theme.borderRadius}px;
 `
 
@@ -45,6 +52,25 @@ export function VideoCard({ appearance, videoUri }: { appearance: Appearance; vi
   const ref = useRef<any>()
 
   const [isPlaying, setIsPlaying] = useState(false)
+
+  if (videoUri.includes('youtube')) {
+    let videoId = videoUri.split('v=')[1]
+    const ampersandPosition = videoId.indexOf('&')
+    if (ampersandPosition !== -1) {
+      videoId = videoId.substring(0, ampersandPosition)
+    }
+    return (
+      <YouTubeVideoSource
+        width="100%"
+        height="100%"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        appearance={appearance}
+      />
+    )
+  }
 
   return (
     <Video appearance={appearance}>
