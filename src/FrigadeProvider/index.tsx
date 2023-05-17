5699762b428657ad42cb69316aa06ad755b28a7b
christianmat <christian_mathiesen@alumni.brown.edu>
2023-05-17 13:58:02 -0700

    make navigate in config optional

--- src/FrigadeProvider/index.tsx
+++ src/FrigadeProvider/index.tsx
@@ -81,7 +81,7 @@ interface FrigadeConfig {
    * This is useful if you are using a router and want to avoid doing a full page refresh on navigation.
    * @param url The url to navigate to.
    */
-  navigate: (url: string, target: string) => void
+  navigate?: (url: string, target: string) => void
   /**
    * Default Appearance for all flows.
    */
