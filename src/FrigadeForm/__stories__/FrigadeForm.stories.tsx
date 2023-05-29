eef4855d65b4fad160c2ad72c44e6844df41c178
Micah Snyder <micah@frigade.com>
2023-05-29 16:07:14 -0700

    fix: Added form pagination

---
+++ src/FrigadeForm/__stories__/FrigadeForm.stories.tsx
@@ -0,0 +1,15 @@
+import { FrigadeForm } from '../index'
+
+export default {
+  title: 'Form',
+  component: FrigadeForm,
+}
+
+export const Announcement = {
+  args: {
+    flowId: 'flow_qmd7eFmco9aTJmf8',
+    type: 'modal',
+    modalPosition: 'center',
+    showPagination: true,
+  },
+}
