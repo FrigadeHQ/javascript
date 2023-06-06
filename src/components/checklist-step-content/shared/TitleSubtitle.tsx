6163d7d2b09e04234d9abfa2d418147ef79b9dd4
Micah Snyder <micah@frigade.com>
2023-06-06 15:45:54 -0700

    Add classnames to hero checklist title and subtitle

--- src/components/checklist-step-content/shared/TitleSubtitle.tsx
+++ src/components/checklist-step-content/shared/TitleSubtitle.tsx
@@ -6,16 +6,19 @@ import {
 } from '../../Checklists/HeroChecklist/styled'
 import { StepContentProps } from '../../../FrigadeForm/types'
 import { sanitize } from '../../../shared/sanitizer'
+import { getClassName } from '../../../shared/appearance'
 
 export const TitleSubtitle: FC<StepContentProps> = ({ stepData, appearance }) => {
   return (
     <>
       <HeroChecklistStepTitle
         appearance={appearance}
+        className={getClassName('checklistStepTitle', appearance)}
         dangerouslySetInnerHTML={sanitize(stepData.title)}
       />
       <HeroChecklistStepSubtitle
         appearance={appearance}
+        className={getClassName('checklistStepSubtitle', appearance)}
         dangerouslySetInnerHTML={sanitize(stepData.subtitle)}
       />
     </>
