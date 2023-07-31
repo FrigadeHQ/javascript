00bb3f8f4e0ef0aaa209291c48486024b4a180f5
Micah Snyder <micah@frigade.com>
2023-07-31 13:35:26 -0700

    fix: Added new deepmerge function

---
+++ src/shared/deepmerge.test.ts
@@ -0,0 +1,67 @@
+import { deepmerge } from './deepmerge'
+
+describe('deepmerge', () => {
+  it('only merges objects', () => {
+    const objectA = { foo: null }
+    const notAnObjectA = null
+    const notAnObjectB = 'foo'
+
+    expect(() => deepmerge(objectA, notAnObjectA)).toThrow()
+    expect(() => deepmerge(objectA, notAnObjectB)).toThrow()
+    expect(() => deepmerge(notAnObjectA, objectA)).toThrow()
+    expect(() => deepmerge(notAnObjectB, objectA)).toThrow()
+    expect(() => deepmerge(notAnObjectA, notAnObjectB)).toThrow()
+  })
+
+  it('merges flat objects', () => {
+    const objectA = { foo: null }
+    const objectB = { bar: 1 }
+    const objectC = { foo: null, bar: 1 }
+
+    const result = deepmerge(objectA, objectB)
+
+    expect(result).toStrictEqual(objectC)
+  })
+
+  it('merges deep objects', () => {
+    const objectA = { foo: null, bar: { baz: null } }
+    const objectB = { quux: 1, bar: { fnord: 2 } }
+    const objectC = { foo: null, bar: { baz: null, fnord: 2 }, quux: 1 }
+
+    const result = deepmerge(objectA, objectB)
+
+    expect(result).toStrictEqual(objectC)
+  })
+
+  it('does not alter source or target objects', () => {
+    const objectA = { foo: null, bar: { baz: null } }
+    const objectB = { quux: 1, bar: { fnord: 2 } }
+
+    deepmerge(objectA, objectB)
+
+    expect(objectA).toStrictEqual({ foo: null, bar: { baz: null } })
+    expect(objectB).toStrictEqual({ quux: 1, bar: { fnord: 2 } })
+  })
+
+  it('passes objects by value', () => {
+    const objectA = { foo: null, bar: { baz: null } }
+    const objectB = { quux: 1, bar: { fnord: 2 } }
+
+    const result = deepmerge(objectA, objectB)
+
+    result.bar.baz = 'changed'
+    result.bar.fnord = 'alsoChanged'
+
+    expect(objectA.bar.baz).toStrictEqual(null)
+    expect(objectB.bar.fnord).toStrictEqual(2)
+  })
+
+  it('merges child arrays', () => {
+    const objectA = { foo: null, bar: ['baz'] }
+    const objectB = { quux: 1, bar: ['fnord'] }
+
+    const result = deepmerge(objectA, objectB)
+
+    expect(result.bar).toStrictEqual(['baz', 'fnord'])
+  })
+})
---
+++ src/shared/deepmerge.ts
@@ -0,0 +1,51 @@
+import structuredClone from 'core-js-pure/actual/structured-clone'
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null && !Array.isArray(obj)
+}
+
+export function deepmerge(...args) {
+  const target = args.shift()
+
+  // Recurse to the right until we've merged all the way back to a single target and source
+  if (args.length > 1) {
+    return deepmerge(...args)
+  }
+
+  const source = args[0]
+
+  if (!isObject(target) || !isObject(source)) {
+    throw new Error('deepmerge can only merge Objects')
+  }
+
+  const result = structuredClone(target)
+
+  Object.entries(source).forEach(([key, value]) => {
+    // Is value an Object, i.e. are we at risk of passing by reference?
+    if (isObject(value)) {
+      // Does key already exist in result?
+      if (result[key] !== undefined) {
+        // Deepmerge it on
+        Object.assign(result, { [key]: deepmerge(result[key], structuredClone(value)) })
+      } else {
+        // Else clone value onto result
+        Object.assign(result, { [key]: structuredClone(value) })
+      }
+    } else if (Array.isArray(value)) {
+      // Does key already exist in result?
+      if (result[key] !== undefined) {
+        // Clone and spread onto existing
+        // TODO: This only shallow-merges arrays. Add ability to deep merge here, so nested structures aren't overwritten
+        Object.assign(result, { [key]: [...result[key], ...structuredClone(value)] })
+      } else {
+        // Else spread array onto result
+        Object.assign(result, { [key]: structuredClone(value) })
+      }
+    } else {
+      // Assign value onto result
+      Object.assign(result, { [key]: value })
+    }
+  })
+
+  return result
+}
