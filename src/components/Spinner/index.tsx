341dc9993ca4c54c6e36ae368fce17d69210850c
christianmat <christian_mathiesen@alumni.brown.edu>
2023-08-29 17:29:23 -0700

    fix: typo in spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -4,19 +4,18 @@ export const Spinner = styled.div`
   width: 24px;
   height: 24px;
   border: 3px solid rgba(255, 255, 255, 0.25);
-  border-bottom-color: #FFF;
+  border-bottom-color: #fff;
   border-radius: 50%;
   display: inline-block;
   box-sizing: border-box;
   animation: rotation 0.75s linear infinite;
-}
 
-@keyframes rotation {
-  0% {
-    transform: rotate(0deg);
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
-  100% {
-    transform: rotate(360deg);
-  }
-}
 `
