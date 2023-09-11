41df759d10f5c36a670f380fb338862b3bfcac28
christianmat <christian_mathiesen@alumni.brown.edu>
2023-09-11 13:21:55 +0200

    fix: support <code/> blocks in html

--- src/shared/sanitizer.ts
+++ src/shared/sanitizer.ts
@@ -3,7 +3,7 @@ import DOMPurify from 'dompurify'
 export function sanitize(dirty: string) {
   return {
     __html: DOMPurify.sanitize(dirty, {
-      ALLOWED_TAGS: ['b', 'i', 'a', 'span', 'div', 'p', 'pre', 'u', 'br', 'img'],
+      ALLOWED_TAGS: ['b', 'i', 'a', 'span', 'div', 'p', 'pre', 'u', 'br', 'img', 'code'],
       ALLOWED_ATTR: ['style', 'class', 'target', 'id', 'href', 'alt', 'src'],
     }),
   }
