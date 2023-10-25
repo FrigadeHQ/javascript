4d5d9cbadb20425d21b388908d29cbfd54aad41a
Micah Snyder <micah@frigade.com>
2023-10-25 10:16:28 -0700

    Add storybook-static to .gitignore

--- apps/storybook/storybook-static/assets/preview-c9540b5d.js
+++
@@ -1,2 +0,0 @@
-const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{o as default};
-//# sourceMappingURL=preview-c9540b5d.js.map
