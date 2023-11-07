cad8fbd427ec3bd103d33ddf4ea463e423b1e39b
Micah Snyder <micah@frigade.com>
2023-11-07 12:09:43 -0800

    Clean up unused dev dependencies in v2

--- packages/reactv2/jest.config.js
+++ packages/reactv2/jest.config.js
@@ -2,12 +2,5 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   setupFilesAfterEnv: ['./jest.setup.js'],
-  testPathIgnorePatterns: ['/__helpers__/'],
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-  },
 }
