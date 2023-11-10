9e00482daf59fdd26ce45b197a7d4b195036e086
christianmat <christian_mathiesen@alumni.brown.edu>
2023-11-10 11:53:26 -0800

    remove wrong command

--- packages/reactv2/release-alpha.sh
+++ packages/reactv2/release-alpha.sh
@@ -5,5 +5,4 @@ yarn install && yarn build && npm publish --tag alpha
 git checkout ../../package.json
 git checkout ../../yarn.lock
 git checkout package.json
-git checkout yarn.lock
 echo 'Alpha successfully released!'
--- packages/reactv2/release-local.sh
+++ packages/reactv2/release-local.sh
@@ -1,9 +1,8 @@
 sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
 sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
 sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
-yarn install && yarn link && yarn local-release
+yarn install && yarn local-release
 git checkout ../../package.json
 git checkout ../../yarn.lock
 git checkout package.json
-git checkout yarn.lock
 echo 'Local build successfully released!'
