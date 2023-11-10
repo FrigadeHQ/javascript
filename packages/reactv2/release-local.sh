ca4c200183d5fe2673e7df0786d32d24bdd99849
christianmat <christian_mathiesen@alumni.brown.edu>
2023-11-10 11:46:50 -0800

    add yarn link

--- packages/reactv2/release-local.sh
+++ packages/reactv2/release-local.sh
@@ -1,7 +1,7 @@
 sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
 sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
 sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
-yarn install && yarn local-release
+yarn install && yarn link && yarn local-release
 git checkout ../../package.json
 git checkout ../../yarn.lock
 git checkout package.json
