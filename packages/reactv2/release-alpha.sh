7ac95c8324e7de2c235e45921e8cb84ef385e1d7
christianmat <christian_mathiesen@alumni.brown.edu>
2023-11-10 11:44:10 -0800

    update scripts

--- packages/reactv2/release-alpha.sh
+++ packages/reactv2/release-alpha.sh
@@ -3,5 +3,7 @@ sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
 sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
 yarn install && yarn build && npm publish --tag alpha
 git checkout ../../package.json
+git checkout ../../yarn.lock
 git checkout package.json
+git checkout yarn.lock
 echo 'Alpha successfully released!'
--- packages/reactv2/release-local.sh
+++ packages/reactv2/release-local.sh
@@ -3,5 +3,7 @@ sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
 sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
 yarn install && yarn local-release
 git checkout ../../package.json
+git checkout ../../yarn.lock
 git checkout package.json
+git checkout yarn.lock
 echo 'Local build successfully released!'
