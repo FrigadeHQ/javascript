07938bef596b483ebddf6b8d0269d4727ece0bf0
christianmat <christian_mathiesen@alumni.brown.edu>
2023-11-20 18:13:19 -0800

    refactor user flow state order

--- packages/js-api/src/core/frigade.ts
+++ packages/js-api/src/core/frigade.ts
@@ -116,43 +116,46 @@ export class Frigade extends Fetchable {
 
   private async refreshUserFlowStates(): Promise<void> {
     const globalStateKey = getGlobalStateKey(this.config)
-    const that = this
 
-    let validator = {
-      set: function (target: any, key: any, value: any) {
-        if (
-          (target[key] &&
-            target[key]?.flowState &&
-            JSON.stringify(target[key].flowState) !== JSON.stringify(value.flowState)) ||
-          JSON.stringify(target[key]?.stepStates) !== JSON.stringify(value.stepStates) ||
-          JSON.stringify(target[key].shouldTrigger) !== JSON.stringify(value.shouldTrigger)
-        ) {
-          that.triggerEventHandlers(target[key], value)
+    if (!frigadeGlobalState[globalStateKey]) {
+      const that = this
+
+      let validator = {
+        set: function (target: any, key: any, value: any) {
+          if (
+            (target[key] &&
+              target[key]?.flowState &&
+              JSON.stringify(target[key].flowState) !== JSON.stringify(value.flowState)) ||
+            JSON.stringify(target[key]?.stepStates) !== JSON.stringify(value.stepStates) ||
+            JSON.stringify(target[key].shouldTrigger) !== JSON.stringify(value.shouldTrigger)
+          ) {
+            that.triggerEventHandlers(target[key], value)
+          }
+
+          target[key] = value
+          return true
+        },
+      }
+
+      frigadeGlobalState[globalStateKey] = {
+        refreshUserFlowStates: async () => {},
+        userFlowStates: new Proxy({}, validator),
+      }
+      frigadeGlobalState[globalStateKey].refreshUserFlowStates = async () => {
+        const userFlowStatesRaw = await this.fetch(
+          `/userFlowStates?foreignUserId=${this.config.userId}${
+            this.config.organizationId ? `&foreignUserGroupId=${this.config.organizationId}` : ''
+          }`
+        )
+        if (userFlowStatesRaw && userFlowStatesRaw.data) {
+          let userFlowStates = userFlowStatesRaw.data as UserFlowState[]
+          userFlowStates.forEach((userFlowState) => {
+            frigadeGlobalState[globalStateKey].userFlowStates[userFlowState.flowId] = userFlowState
+          })
         }
-
-        target[key] = value
-        return true
-      },
-    }
-
-    frigadeGlobalState[globalStateKey] = {
-      refreshUserFlowStates: async () => {},
-      userFlowStates: new Proxy({}, validator),
-    }
-
-    frigadeGlobalState[globalStateKey].refreshUserFlowStates = async () => {
-      const userFlowStatesRaw = await this.fetch(
-        `/userFlowStates?foreignUserId=${this.config.userId}${
-          this.config.organizationId ? `&foreignUserGroupId=${this.config.organizationId}` : ''
-        }`
-      )
-      if (userFlowStatesRaw && userFlowStatesRaw.data) {
-        let userFlowStates = userFlowStatesRaw.data as UserFlowState[]
-        userFlowStates.forEach((userFlowState) => {
-          frigadeGlobalState[globalStateKey].userFlowStates[userFlowState.flowId] = userFlowState
-        })
       }
     }
+
     await frigadeGlobalState[globalStateKey].refreshUserFlowStates()
   }
 
