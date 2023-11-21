ed183fbfe3322d59a8ec2934dcee6d0f72a83579
Christian Mathiesen <christian@frigade.com>
2023-11-21 14:08:53 -0800

    feat(reactv2): automatic refreshes of useFlow hook (#92)
    * feat(reactv2): automatic refreshes of useFlow hook
    
    * fix handler

--- packages/reactv2/src/hooks/useFlow.ts
+++ packages/reactv2/src/hooks/useFlow.ts
@@ -1,21 +1,29 @@
-import { Frigade, Flow } from '@frigade/js'
-import { useContext, useState } from 'react'
+import { Flow, Frigade } from '@frigade/js'
+import { useContext, useEffect, useState } from 'react'
 
 import { FrigadeContext } from '../components/Provider'
 
 export function useFlow(flowId: string) {
   const [flow, setFlow] = useState<Flow>(null)
   const { apiKey, config } = useContext(FrigadeContext)
+  const filteredConfig = Object.fromEntries(
+    Object.entries(config).filter(([k, v]) => ['apiUrl', 'userId'].includes(k) && v != null)
+  )
+  const frigade = new Frigade(apiKey, filteredConfig)
+
+  useEffect(() => {
+    const handler = (flow: Flow) => {
+      setFlow(flow)
+    }
+
+    frigade.onFlowStateChange(handler)
+    return () => {
+      frigade.removeOnFlowStateChangeHandler(handler)
+    }
+  }, [])
 
   async function fetchFlow() {
-    const filteredConfig = Object.fromEntries(
-      Object.entries(config).filter(([k, v]) => ['apiUrl', 'userId'].includes(k) && v != null)
-    )
-
-    const frigade = await new Frigade(apiKey, filteredConfig)
-
     const flowResponse: Flow = await frigade.getFlow(flowId)
-
     setFlow(flowResponse)
   }
 
@@ -23,7 +31,5 @@ export function useFlow(flowId: string) {
     fetchFlow()
   }
 
-  // TEMP: Expose a way to manually refresh the flow.
-  // TODO: Automatically update state when something like step.complete() is called
   return { flow, fetchFlow }
 }
