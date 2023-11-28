30223e6af6741d46bf80a708a848b6d84429136f
Micah Snyder <micah@frigade.com>
2023-11-28 11:15:41 -0800

    Add missing font weights

--- packages/reactv2/src/shared/tokens/scalarTokens.ts
+++ packages/reactv2/src/shared/tokens/scalarTokens.ts
@@ -112,9 +112,15 @@ export const scalarTokens = {
     '5xl': '48px',
   },
   fontWeights: {
+    thin: '100',
+    extralight: '200',
+    light: '300',
     regular: '400',
+    medium: '500',
     demibold: '600',
     bold: '700',
+    extrabold: '800',
+    black: '900',
   },
   letterSpacings: {
     md: '0.02em',
