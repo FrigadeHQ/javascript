295b4777aa348dbeaf3576aa2e3cd59ca90a30e1
christianmat <christian_mathiesen@alumni.brown.edu>
2023-12-04 14:25:37 -0800

    dedupe identical api calls

--- packages/js-api/src/shared/utils.ts
+++ packages/js-api/src/shared/utils.ts
@@ -16,6 +16,9 @@ const LAST_POST_CALL_AT = 'frigade-last-call-at-'
 const LAST_POST_CALL_DATA = 'frigade-last-call-data-'
 const GUEST_KEY = 'frigade-guest-key'
 const GUEST_PREFIX = 'guest_'
+const GET_CACHE_PREFIX = 'get-cache-'
+const GET_CACHE_TTL_MS = 1000
+const POST_CACHE_TTL_MS = 1000
 
 export function cloneFlow(flow: Flow): Flow {
   const newFlow = new Flow(flow.config, flow.rawData)
@@ -43,20 +46,30 @@ export function getHeaders(apiKey: string) {
 }
 
 function getLocalStorage(key: string) {
-  if (window && window.localStorage) {
+  if (isWeb()) {
     return window.localStorage.getItem(key)
   }
   return null
 }
 
 function setLocalStorage(key: string, value: string) {
-  if (window && window.localStorage) {
+  if (isWeb()) {
     window.localStorage.setItem(key, value)
   }
 }
 
+function clearAllGetCache() {
+  if (isWeb()) {
+    Object.keys(window.localStorage).forEach((key) => {
+      if (key.startsWith(GET_CACHE_PREFIX)) {
+        window.localStorage.removeItem(key)
+      }
+    })
+  }
+}
+
 export function resetAllLocalStorage() {
-  if (window && window.localStorage) {
+  if (isWeb()) {
     // Clear all local storage items that begin with `frigade-`
     Object.keys(window.localStorage).forEach((key) => {
       if (key.startsWith('frigade-')) {
@@ -69,7 +82,7 @@ export function resetAllLocalStorage() {
 export async function gracefulFetch(url: string, options: any) {
   const lastCallAtKey = LAST_POST_CALL_AT + url
   const lastCallDataKey = LAST_POST_CALL_DATA + url
-  if (window && window.localStorage && options && options.body && options.method === 'POST') {
+  if (isWeb() && options && options.body && options.method === 'POST') {
     const lastCall = getLocalStorage(lastCallAtKey)
     const lastCallData = getLocalStorage(lastCallDataKey)
     if (lastCall && lastCallData && lastCallData == options.body) {
@@ -77,12 +90,26 @@ export async function gracefulFetch(url: string, options: any) {
       const now = new Date()
       const diff = now.getTime() - lastCallDate.getTime()
       // Throttle consecutive POST calls to 1 second
-      if (diff < 1000) {
+      if (diff < POST_CACHE_TTL_MS) {
         return getEmptyResponse()
       }
     }
     setLocalStorage(lastCallAtKey, new Date().toISOString())
     setLocalStorage(lastCallDataKey, options.body)
+    clearAllGetCache()
+  }
+
+  const isGetCall = options?.method === 'GET' || !options?.method
+  if (isWeb() && isGetCall) {
+    const cachedResponse = getLocalStorage(`${GET_CACHE_PREFIX}${url}`)
+    if (cachedResponse) {
+      const parsedResponse = JSON.parse(cachedResponse)
+      const now = new Date()
+      const diff = now.getTime() - parsedResponse.timestamp
+      if (diff < GET_CACHE_TTL_MS) {
+        return parsedResponse.response
+      }
+    }
   }
 
   let response
@@ -100,7 +127,17 @@ export async function gracefulFetch(url: string, options: any) {
     return getEmptyResponse(response.statusText)
   }
 
-  return response.json()
+  const responseJson = await response.json()
+
+  // If the call is a GET, cache the response in local storage
+  if (isWeb() && isGetCall) {
+    setLocalStorage(
+      `${GET_CACHE_PREFIX}${url}`,
+      JSON.stringify({ timestamp: new Date().getTime(), response: responseJson })
+    )
+  }
+
+  return responseJson
 }
 
 function getEmptyResponse(error?: any) {
@@ -115,7 +152,7 @@ function getEmptyResponse(error?: any) {
 }
 
 export function generateGuestId() {
-  if (typeof window !== 'undefined' && typeof window.localStorage !== 'undefined') {
+  if (isWeb()) {
     let guestId = getLocalStorage(GUEST_KEY)
     if (!guestId) {
       guestId = `${GUEST_PREFIX}${uuidv4()}`
@@ -130,4 +167,8 @@ export function fetcher(apiKey: string, path: string, options?: Record<any, any>
     ...(options ?? {}),
     ...getHeaders(apiKey),
   })
+}
+
+function isWeb() {
+  return typeof window !== 'undefined'
 }
