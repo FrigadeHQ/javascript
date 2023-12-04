5500a8a34a04a180bcf677d016fa28f501b586f6
christianmat <christian_mathiesen@alumni.brown.edu>
2023-12-04 15:39:33 -0800

    feat(html): Allow HTML in strings in ReactV2

---
+++ packages/reactv2/src/shared/sanitize.ts
@@ -0,0 +1,50 @@
+import DOMPurify from 'dompurify'
+
+export function sanitize(dirty?: string) {
+  if (!dirty) {
+    return { __html: '' }
+  }
+  return {
+    __html: DOMPurify.sanitize(dirty, {
+      ALLOWED_TAGS: [
+        'b',
+        'i',
+        'a',
+        'span',
+        'div',
+        'p',
+        'pre',
+        'u',
+        'br',
+        'img',
+        'code',
+        'li',
+        'ul',
+        'table',
+        'tbody',
+        'thead',
+        'tr',
+        'td',
+        'th',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'video',
+      ],
+      ALLOWED_ATTR: [
+        'style',
+        'class',
+        'target',
+        'id',
+        'href',
+        'alt',
+        'src',
+        'controls',
+        'autoplay',
+        'loop',
+        'muted',
+      ],
+    }),
+  }
+}
