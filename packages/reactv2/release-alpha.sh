dba51a4df05f26109845b91642993dc205f41c83
christianmat <christian_mathiesen@alumni.brown.edu>
2024-01-04 21:29:53 -0800

    fix build script to avoid accidental npm updates

--- packages/reactv2/release-alpha.sh
+++ packages/reactv2/release-alpha.sh
@@ -1,6 +1,7 @@
 sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
 sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
 sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
+sed -i '' '/"private": true,/d' package.json
 yarn install && yarn build && npm publish --tag alpha
 git checkout ../../package.json
 git checkout ../../yarn.lock
