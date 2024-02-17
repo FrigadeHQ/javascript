db8875a99b48266d4a97497d830db663b78e16e5
Vansh Makh <vansh.p.singh@gmail.com>
2024-02-16 17:28:22 -0800

    Update Video.tsx to include Loom video support

--- packages/reactv2/src/components/Media/Video.tsx
+++ packages/reactv2/src/components/Media/Video.tsx
@@ -13,9 +13,13 @@ function getVideoEmbedSrc(videoUri: string) {
     const videoId = videoUri.split('wistia.com/medias/')[1]?.split('&')[0]
 
     return `https://fast.wistia.net/embed/iframe/${videoId}`
+  } else if (videoUri.includes('loom')) {
+    const videoId = videoUri.split('loom.com/share/')[1]?.split('&')[0]
+
+    return `https://loom.com/embed/${videoId}?hideEmbedTopBar=true&hide_title=true&hide_share=true&hide_owner=true`
   }
 
-  throw new Error('Could not map videoUri to a known provider (Youtube, Vimeo, Wistia).')
+  throw new Error('Could not map videoUri to a known provider (Youtube, Vimeo, Wistia, Loom).')
 }
 
 export interface VideoProps extends BoxProps {
