0d63725c008bcaef1793ba40738812ef853d8878
christianmat <christian_mathiesen@alumni.brown.edu>
2024-02-19 19:19:08 -0800

    fix: add todo for script

--- packages/reactv1/release-legacy.sh
+++ packages/reactv1/release-legacy.sh
@@ -1,9 +1,10 @@
-sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
-sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
-sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
-sed -i '' '/"private": true,/d' package.json
-yarn install && yarn build && npm publish --tag alpha
-git checkout ../../package.json
-git checkout ../../yarn.lock
-git checkout package.json
-echo 'Alpha successfully released!'
+# TODO: Implement this script for patching old versions of reactv1
+#sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
+#sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
+#sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
+#sed -i '' '/"private": true,/d' package.json
+#yarn install && yarn build && npm publish --tag alpha
+#git checkout ../../package.json
+#git checkout ../../yarn.lock
+#git checkout package.json
+#echo 'Alpha successfully released!'
