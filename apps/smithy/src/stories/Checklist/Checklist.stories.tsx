84b721e5c737b66cd68a3093e222d6ef5abee588
Micah Snyder <micah@frigade.com>
2024-02-22 16:56:41 -0800

    Clean up names & types

--- apps/smithy/src/stories/Checklist/Checklist.stories.tsx
+++ apps/smithy/src/stories/Checklist/Checklist.stories.tsx
@@ -1,13 +1,51 @@
-import { Checklist } from "@frigade/react";
+import {
+  Card,
+  Checklist,
+  type CollapsibleStepProps,
+  Flex,
+  Text,
+} from "@frigade/react";
 
 export default {
   title: "Components/Checklist",
-  component: Checklist,
+  component: Checklist.Collapsible,
 };
+
+const { CollapsibleStep } = Checklist;
+
+function TestStep({
+  handlePrimary,
+  handleSecondary,
+  open,
+  onOpenChange,
+  step,
+}: CollapsibleStepProps) {
+  const { isCompleted, primaryButtonTitle, secondaryButtonTitle, title } = step;
+
+  return (
+    <CollapsibleStep.Root open={open} onOpenChange={onOpenChange}>
+      <CollapsibleStep.Trigger isCompleted={isCompleted} title={title} />
+
+      <CollapsibleStep.Content>
+        <Text.H1>CUSTOM STEPPPPPP</Text.H1>
+        <Flex.Row gap={3}>
+          <Card.Secondary
+            title={secondaryButtonTitle}
+            onClick={handleSecondary}
+          />
+          <Card.Primary title={primaryButtonTitle} onClick={handlePrimary} />
+        </Flex.Row>
+      </CollapsibleStep.Content>
+    </CollapsibleStep.Root>
+  );
+}
 
 export const Default = {
   args: {
     flowId: "flow_K2dmIlteW8eGbGoo",
+    stepTypes: {
+      test: TestStep,
+    },
     width: "500px",
   },
 };
---
+++ packages/react/src/components/Checklist/Collapsible.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react'
+
+import { Card } from '@/components/Card'
+import { Flex } from '@/components/Flex'
+import { Flow, type FlowChildrenProps, type FlowPropsWithoutChildren } from '@/components/Flow'
+
+import * as CollapsibleStep from '@/components/Checklist/CollapsibleStep'
+
+export interface CollapsibleProps extends FlowPropsWithoutChildren {
+  stepTypes: Record<string, (props: FlowChildrenProps) => React.ReactNode>
+}
+
+export interface CollapsibleStepProps extends FlowChildrenProps {
+  onOpenChange: (isOpening: boolean) => void
+  open: boolean
+}
+
+function DefaultCollapsibleStep({
+  handlePrimary,
+  handleSecondary,
+  open,
+  onOpenChange,
+  step,
+}: CollapsibleStepProps) {
+  const { isCompleted, primaryButtonTitle, secondaryButtonTitle, subtitle, title } = step
+
+  return (
+    <CollapsibleStep.Root open={open} onOpenChange={onOpenChange}>
+      <CollapsibleStep.Trigger isCompleted={isCompleted} title={title} />
+
+      <CollapsibleStep.Content>
+        <Card.Media aspectRatio={2.5} objectFit="cover" src={step.imageUri} />
+        <Card.Subtitle color="gray500">{subtitle}</Card.Subtitle>
+        <Flex.Row gap={3}>
+          <Card.Secondary title={secondaryButtonTitle} onClick={handleSecondary} />
+          <Card.Primary title={primaryButtonTitle} onClick={handlePrimary} />
+        </Flex.Row>
+      </CollapsibleStep.Content>
+    </CollapsibleStep.Root>
+  )
+}
+
+const defaultStepTypes = {
+  default: DefaultCollapsibleStep,
+}
+
+interface ChecklistContentProps extends FlowChildrenProps, Pick<CollapsibleProps, 'stepTypes'> {}
+
+function ChecklistContent({ flow, step, stepTypes, ...props }: ChecklistContentProps) {
+  const [openStepId, setOpenStepId] = useState(step.id)
+
+  useEffect(() => {
+    setOpenStepId(step.id)
+  }, [step.id])
+
+  const mergedStepTypes = {
+    ...defaultStepTypes,
+    ...stepTypes,
+  }
+
+  const stepList = Array.from(flow.steps?.entries()).map(([, s]) => {
+    const StepComponent = mergedStepTypes[s.type as string] ?? DefaultCollapsibleStep
+
+    function onOpenChange(isOpening: boolean) {
+      setOpenStepId(isOpening ? s.id : null)
+    }
+
+    return (
+      <StepComponent
+        flow={flow}
+        key={s.id}
+        onOpenChange={onOpenChange}
+        open={s.id === openStepId}
+        step={s}
+        {...props}
+      />
+    )
+  })
+
+  return (
+    <>
+      <Flex.Column gap={2}>
+        <Card.Title>{flow.title}</Card.Title>
+        <Card.Subtitle color="gray500">{flow.subtitle}</Card.Subtitle>
+      </Flex.Column>
+
+      {stepList}
+    </>
+  )
+}
+
+export function Collapsible({ flowId, stepTypes = {}, ...props }: CollapsibleProps) {
+  return (
+    <Flow as={Card} borderWidth="md" flowId={flowId} part="checklist" {...props}>
+      {(childrenProps) => <ChecklistContent stepTypes={stepTypes} {...childrenProps} />}
+    </Flow>
+  )
+}
