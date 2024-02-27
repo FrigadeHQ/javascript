f33d5e95e0602257196acd16dac2da00997329cb
christianmat <christian_mathiesen@alumni.brown.edu>
2024-02-27 11:14:44 -0800

    fix: version number

--- packages/reactv1/release-legacy.sh
+++ packages/reactv1/release-legacy.sh
@@ -1,8 +1,8 @@
 
-sed -i '' "s/apps\/\*/packages\/reactv1/g" ../../package.json
+sed -i '' "s/frigade\/react/frigade\/reactv2/g" ../react/package.json
 sed -i '' "s/frigade\/reactv1/frigade\/react/g" package.json
 sed -i '' '/"private": true,/d' package.json
-yarn install && yarn build && npm publish --dry-run
+yarn install && yarn build && npm publish --tag legacy
 git checkout ../../package.json
 git checkout ../../yarn.lock
 git checkout package.json
