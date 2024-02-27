eefc6b52228d8bd54ef918af5de74a4e774cc343
christianmat <christian_mathiesen@alumni.brown.edu>
2024-02-27 11:05:41 -0800

    fix: updated legacy release script

--- packages/reactv1/release-legacy.sh
+++ packages/reactv1/release-legacy.sh
@@ -1,10 +1,9 @@
-# TODO: Implement this script for patching old versions of reactv1
-#sed -i '' "s/apps\/\*/packages\/reactv2/g" ../../package.json
-#sed -i '' "s/packages\/\*/packages\/js-api/g" ../../package.json
-#sed -i '' "s/frigade\/reactv2/frigade\/react/g" package.json
-#sed -i '' '/"private": true,/d' package.json
-#yarn install && yarn build && npm publish --tag alpha
-#git checkout ../../package.json
-#git checkout ../../yarn.lock
-#git checkout package.json
-#echo 'Alpha successfully released!'
+
+sed -i '' "s/apps\/\*/packages\/reactv1/g" ../../package.json
+sed -i '' "s/frigade\/reactv1/frigade\/react/g" package.json
+sed -i '' '/"private": true,/d' package.json
+yarn install && yarn build && npm publish --dry-run
+git checkout ../../package.json
+git checkout ../../yarn.lock
+git checkout package.json
+echo 'Legacy version successfully released!'
