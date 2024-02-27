80ca488e8dc6df8f61e455a9d70a1b3d95233b91
christianmat <christian_mathiesen@alumni.brown.edu>
2024-02-27 11:24:31 -0800

    fix: clean up new package json

--- packages/reactv1/release-legacy.sh
+++ packages/reactv1/release-legacy.sh
@@ -4,6 +4,7 @@ sed -i '' "s/frigade\/reactv1/frigade\/react/g" package.json
 sed -i '' '/"private": true,/d' package.json
 yarn install && yarn build && npm publish --tag legacy
 git checkout ../../package.json
+git checkout ../react/package.json
 git checkout ../../yarn.lock
 git checkout package.json
 echo 'Legacy version successfully released!'
