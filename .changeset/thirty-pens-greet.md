8454f4457e203680f72140edfcaf302038cf4b49
christianmat <christian_mathiesen@alumni.brown.edu>
2024-03-04 17:00:30 -0800

    changeset: progress dot class names

---
+++ .changeset/thirty-pens-greet.md
@@ -0,0 +1,5 @@
+---
+"@frigade/react": patch
+---
+
+Adds class name selectors on progress dots
--- packages/react/src/components/Dialog/index.tsx
+++ packages/react/src/components/Dialog/index.tsx
@@ -84,12 +84,14 @@ Dialog.ProgressDots = ({ current, total }: { current: number; total: number }) =
 
   const dots = [...Array(total)].map((_, i) => {
     return (
-      <circle
+      <Box
+        as="circle"
         key={i}
         r={4}
         cx={4 + 16 * i}
         cy="4px"
         fill={current === i ? theme.colors.blue500 : theme.colors.blue800}
+        part={current === i ? 'progress-dot-selected' : 'progress-dot'}
       />
     )
   })
