359fea698783d0b3e1617d6480b7f6937c1d3d27
christianmat <christian_mathiesen@alumni.brown.edu>
2024-04-01 18:57:15 -0700

    switch to random id

--- apps/smithy/.storybook/preview.jsx
+++ apps/smithy/.storybook/preview.jsx
@@ -13,10 +13,10 @@ const FRIGADE_API_KEY =
   "api_public_GY6O5JS99XTL2HAXU0D6OQHYQ7I706P5I9C9I7CEZFNFUFRARD2DVDSMFW3YT3SV";
 const API_URL = "https://api.frigade.com";
 
-const userId = `jonathan_livingston_smeagol_llxsccbb`;
-// const userId = `jonathan_livingston_smeagol_${Math.random()
-//   .toString(36)
-//   .substring(2, 9)}`;
+// const userId = `jonathan_livingston_smeagol`;
+const userId = `jonathan_livingston_smeagol_${Math.random()
+  .toString(36)
+  .substring(2, 9)}`;
 
 const preview = {
   parameters: {
