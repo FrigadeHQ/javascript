aebda3a55f57380024d7e384ab0f624674ada722
christianmat <christian_mathiesen@alumni.brown.edu>
2024-04-09 13:35:20 -0700

    fix: undefined enums at runtime

---
+++ .changeset/itchy-seals-explode.md
@@ -0,0 +1,5 @@
+---
+"@frigade/js": patch
+---
+
+Fixes an issue where enums were undefined at runtime
--- packages/js-api/src/index.ts
+++ packages/js-api/src/index.ts
@@ -1,4 +1,4 @@
 export { Frigade } from './core/frigade'
 export { Flow } from './core/flow'
-export type { FlowStatus, FlowStep, FrigadeConfig, FlowType, TriggerType } from './core/types'
+export { FlowStatus, type FlowStep, type FrigadeConfig, FlowType, TriggerType } from './core/types'
 export type { RulesGraphRegistryCallback } from './core/rules-graph'
