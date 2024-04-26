3fc3dbbaa037701181bcd971d829e55c8db4b367
Micah Snyder <micah@frigade.com>
2024-04-26 14:29:10 -0700

    Add css prop to Provider

--- packages/react/src/components/Provider/Provider.tsx
+++ packages/react/src/components/Provider/Provider.tsx
@@ -32,34 +32,47 @@ export interface ProviderProps {
    * Your public API key from the Frigade dashboard. Do not ever use your private API key here.
    */
   apiKey: string
+
   /**
    * The URL prefix of the API to use. By default, Frigade will use the production API: https://api.frigade.com/v1/public
    */
   apiUrl?: string
+
   children?: React.ReactNode
+
   /**
-   * A function to handle navigation. By default, Frigade will use `window.open` if not provided.
-   * https://docs.frigade.com/v2/sdk/navigation
+   * Global CSS properties to attach to the :root element.
+   * @see https://emotion.sh/docs/css-prop#object-styles
    */
-  navigate?: NavigateHandler
-  /**
-   * The global theme to use across components. See docs on styling: https://docs.frigade.com/v2/sdk/styling/theming
-   */
-  theme?: Theme
-  /**
-   * The user ID of the user who is interacting with Frigade. If not provided, Frigade will generate a random guest ID and persist it in local storage.
-   */
-  userId?: string
+  css?: Record<string, unknown>
+
   /**
    * The group ID to use for this context (optional).
    */
   groupId?: string
 
   /**
+   * A function to handle navigation. By default, Frigade will use `window.open` if not provided.
+   * https://docs.frigade.com/v2/sdk/navigation
+   */
+  navigate?: NavigateHandler
+
+  /**
+   * The global theme to use across components. See docs on styling: https://docs.frigade.com/v2/sdk/styling/theming
+   */
+  theme?: Theme
+
+  /**
+   * The user ID of the user who is interacting with Frigade. If not provided, Frigade will generate a random guest ID and persist it in local storage.
+   */
+  userId?: string
+
+  /**
    * @ignore Internal use only.
    * If enabled, Frigade will not send any data to the API. A user's state will be reset on page refresh.
    */
   __readOnly?: boolean
+
   /**
    * @ignore Internal use only.
    * Map of Flow ID to Flow State for all flows in the app that should be mocked.
@@ -67,7 +80,7 @@ export interface ProviderProps {
   __flowStateOverrides?: Record<string, StatefulFlow>
 }
 
-export function Provider({ children, navigate, theme, ...props }: ProviderProps) {
+export function Provider({ children, css = {}, navigate, theme, ...props }: ProviderProps) {
   const themeOverrides = theme ? createThemeVariables(theme) : {}
   const [modals, setModals] = useState(new Set<string>())
   const registeredComponents = useRef<RegisteredComponents>(new Map())
@@ -171,7 +184,7 @@ export function Provider({ children, navigate, theme, ...props }: ProviderProps)
         hasInitialized,
       }}
     >
-      <Global styles={{ ':root': { ...themeVariables, ...themeOverrides } }} />
+      <Global styles={{ ':root': { ...themeVariables, ...themeOverrides, ...css } }} />
       <ThemeProvider theme={themeTokens}>{children}</ThemeProvider>
     </FrigadeContext.Provider>
   )
