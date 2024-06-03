c6c286a0bd1468d57ede01daf3dba04dbd580272
christianmat <christian_mathiesen@alumni.brown.edu>
2024-06-03 15:35:20 -0700

    remove state that is not needed

--- packages/react/src/components/Provider/ImagePreloader.tsx
+++ packages/react/src/components/Provider/ImagePreloader.tsx
@@ -1,16 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useFrigade } from '@/hooks/useFrigade'
 
 export function ImagePreloader() {
-  const [hasPreloaded, setHasPreloaded] = useState(false)
   const { frigade } = useFrigade()
 
   useEffect(() => {
     ;(async () => {
-      if (hasPreloaded) {
-        return
-      }
-      setHasPreloaded(true)
       const flows = await frigade.getFlows()
       flows.forEach((flow) => {
         flow.steps.forEach((step) => {
