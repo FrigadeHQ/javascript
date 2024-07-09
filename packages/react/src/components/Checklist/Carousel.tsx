4a8a50b2ba5aa9abcb5d2500c86e491a364bdf1a
Christian Mathiesen <christian@frigade.com>
2024-07-09 02:22:13 +0200

    fix: updates to the carousel component sorting (#311)
    * carousel sorting updates
    
    * sorting -> sort
    
    ---------
    
    Co-authored-by: Micah Snyder <micah@frigade.com>

--- packages/react/src/components/Checklist/Carousel.tsx
+++ packages/react/src/components/Checklist/Carousel.tsx
@@ -1,12 +1,27 @@
 import { Card } from '@/components/Card'
-import { Flow, FlowChildrenProps, type FlowProps } from '@/components/Flow'
+import { Flow, FlowChildrenProps, type FlowPropsWithoutChildren } from '@/components/Flow'
 
 import { CarouselEmblaWrapper } from '@/components/Checklist/CarouselEmblaWrapper'
 
-export function Carousel({ ...props }: FlowProps) {
+export interface CarouselProps extends FlowPropsWithoutChildren {
+  /**
+   * @ignore
+   */
+  children?: React.ReactNode
+  /**
+   * How to sort the default the completed steps of the carousel.
+   * - `completed-last` will sort the completed/skips steps to the end of the carousel.
+   * - `default` will keep the order of the steps as they are in the flow.
+   */
+  sort?: 'completed-last' | 'default'
+}
+
+export function Carousel({ ...props }: CarouselProps) {
   return (
     <Flow as={Card} borderWidth={1} containerType="inline-size" p="4" part="carousel" {...props}>
-      {(flowChildrenProps: FlowChildrenProps) => <CarouselEmblaWrapper {...flowChildrenProps} />}
+      {(flowChildrenProps: FlowChildrenProps) => (
+        <CarouselEmblaWrapper {...props} {...flowChildrenProps} />
+      )}
     </Flow>
   )
 }
