fa00820d3c180b4fcc341d680e08053884485740
Micah Snyder <micah@frigade.com>
2024-08-15 16:42:06 -0700

    Flow component should have no opinion on modal prop

--- packages/react/src/components/Flow/index.tsx
+++ packages/react/src/components/Flow/index.tsx
@@ -38,7 +38,6 @@ export function Flow({
   const {
     dismissible = false,
     forceMount = false,
-    modal = false,
     ...mergedProps
   } = {
     ...(flow?.props ?? {}),
@@ -60,7 +59,7 @@ export function Flow({
   })
 
   const isModal =
-    modal ||
+    mergedProps?.modal ||
     (typeof as === 'function' && as?.displayName === 'Dialog') ||
     [FlowType.ANNOUNCEMENT, FlowType.TOUR].includes(flow?.rawData?.flowType)
 
@@ -115,7 +114,6 @@ export function Flow({
   const ContainerElement = as === null ? Fragment : as ?? Box
 
   const containerProps = {
-    modal,
     ...mergedProps,
     'data-flow-id': flow.id,
   }
