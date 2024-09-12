d829337be24f93e3af994354100d5d9be5cfb791
christianmat <christian_mathiesen@alumni.brown.edu>
2024-09-12 12:30:29 -0700

    fix: remove modals on url change

--- packages/react/src/hooks/useModal.ts
+++ packages/react/src/hooks/useModal.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect } from 'react'
 import type { Flow } from '@frigade/js'
 
-export let globalModalState: Set<string> = new Set()
+export const globalModalState: Set<string> = new Set()
 
 export function useModal(flow: Flow, isModal: boolean = true) {
   const removeModal = useCallback(() => {
@@ -21,6 +21,23 @@ export function useModal(flow: Flow, isModal: boolean = true) {
       removeModal()
     }
   }, [])
+
+  useEffect(() => {
+    registerModal()
+
+    const handleRouteChange = () => {
+      removeModal()
+    }
+
+    window.addEventListener('popstate', handleRouteChange)
+    window.addEventListener('beforeunload', handleRouteChange)
+
+    return () => {
+      removeModal()
+      window.removeEventListener('popstate', handleRouteChange)
+      window.removeEventListener('beforeunload', handleRouteChange)
+    }
+  }, [registerModal, removeModal])
 
   if (!flow?.isVisible) {
     removeModal()
