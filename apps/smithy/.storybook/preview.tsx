021e899d7cb9b3176229048b32664a9672bbcba4
Micah Snyder <28678+dfltr@users.noreply.github.com>
2024-09-17 17:13:31 -0700

    Storybook tests (#370)
    * Add Storybook tests for Banner
    
    * Clean up Banner tests, add Storybook test runner
    
    * Remove console.log

---
+++ apps/smithy/.storybook/preview.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { FrigadeJS, Provider } from "@frigade/react";
+
+import "./global.css";
+
+// LOCAL DEV
+// export const FRIGADE_API_KEY =
+//   "api_public_0FaxqVs527bAVQsFK4RcuJYjVqHeC5U7CGJLfsVXRE36eAKiLjwAEugZYeFijCI2";
+// export const FRIGADE_API_URL = "https://localhost:3443/v1/public";
+
+// FRIGADE DEV
+export const FRIGADE_API_KEY =
+  "api_public_GY6O5JS99XTL2HAXU0D6OQHYQ7I706P5I9C9I7CEZFNFUFRARD2DVDSMFW3YT3SV";
+export const FRIGADE_API_URL = "https://api.frigade.com";
+
+// const userId = `jonathan_livingston_smeagol`;
+export const FRIGADE_USER_ID = `jonathan_livingston_smeagol_${Math.random()
+  .toString(36)
+  .substring(2, 9)}`;
+
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Provider
+          apiKey={FRIGADE_API_KEY}
+          apiUrl={FRIGADE_API_URL}
+          defaultCollection={true}
+          // generateGuestId={false}
+          userId={FRIGADE_USER_ID}
+          // syncOnWindowUpdates={false}
+          userProperties={{
+            firstName: "Jonathan",
+            lastName: "Livingston",
+            name: "Jonathan Livingston",
+            email: "jonathan.livingston@frigade.com",
+            image:
+              "https://cdn.frigade.com/6a05066d-6781-46f9-8476-bbbde3c39960.jpeg",
+          }}
+          variables={{
+            globalFullName: "Balrog Hansen",
+          }}
+          // __readOnly={true}
+          // __flowStateOverrides={{
+          //   flow_ZacoWhZhzqbdHQ8k: {
+          //     flowSlug: "some-flow",
+          //     flowName: "Some flow",
+          //     flowType: FrigadeJS.FlowType.BANNER,
+          //     data: {
+          //       steps: [
+          //         {
+          //           id: "step-one",
+          //           title: "Banner title",
+          //           subtitle: "Banner subtitle",
+          //           primaryButton: { title: "Primary CTA" },
+          //           secondaryButton: { title: "Secondary CTA" },
+          //           imageUri: "https://placehold.co/40x40",
+          //           $state: {
+          //             completed: false,
+          //             started: false,
+          //             visible: true,
+          //             blocked: false,
+          //             skipped: false,
+          //           },
+          //         },
+          //       ],
+          //     },
+          //     $state: {
+          //       currentStepId: "step-one",
+          //       visible: true,
+          //       started: false,
+          //       completed: false,
+          //       skipped: false,
+          //       currentStepIndex: -1,
+          //     },
+          //   },
+          // }}
+        >
+          <Story />
+        </Provider>
+      );
+    },
+  ],
+};
+
+export default preview;
--- apps/smithy/src/stories/Banner/Banner.stories.tsx
+++ apps/smithy/src/stories/Banner/Banner.stories.tsx
@@ -1,17 +1,76 @@
 import { Banner } from "@frigade/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { expect, within } from "@storybook/test";
+
+export type BannerStory = StoryObj<typeof Banner>;
 
 export default {
   title: "Components/Banner",
   component: Banner,
+} as Meta<typeof Banner>;
+
+export const Default: BannerStory = {
+  args: {
+    flowId: "flow_ZacoWhZhzqbdHQ8k",
+  },
+
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+    const bannerElement = await canvas.findByRole("complementary", {
+      name: "Banner",
+    });
+    const banner = within(bannerElement);
+
+    expect(bannerElement).toBeInTheDocument();
+
+    await step("Check Banner contents", async () => {
+      expect(
+        banner.getByRole("heading", { name: "Banner title" })
+      ).toBeVisible();
+
+      // TODO: Add a11y attributes to subtitle
+      expect(banner.getByText("Banner subtitle")).toBeVisible();
+
+      // TODO: Add a11y attributes to image
+      expect(banner.getByRole("img")).toBeVisible();
+
+      expect(banner.getByRole("button", { name: "Primary CTA" })).toBeVisible();
+
+      expect(
+        banner.getByRole("button", { name: "Secondary CTA" })
+      ).toBeVisible();
+    });
+
+    // Check that stable classnames unique to this component exist in the DOM
+    await step("Check SDK classnames", async () => {
+      expect(canvasElement.querySelector(".fr-banner")).toBeInTheDocument();
+
+      expect(
+        canvasElement.querySelector(".fr-banner-title-wrapper")
+      ).toBeInTheDocument();
+    });
+  },
 };
 
-export const Default = {
+export const Dismissible: BannerStory = {
   args: {
     dismissible: true,
     flowId: "flow_ZacoWhZhzqbdHQ8k",
-    onPrimary: () => {
-      console.log("Primary button clicked");
-      return false;
-    },
+  },
+
+  play: async (context) => {
+    const { canvasElement, step } = context;
+
+    const canvas = within(canvasElement);
+    const bannerElement = await canvas.findByRole("complementary", {
+      name: "Banner",
+    });
+    const banner = within(bannerElement);
+
+    await Default.play?.(context);
+
+    await step("Check dismissible state", async () => {
+      expect(banner.getByRole("button", { name: "Dismiss" })).toBeVisible();
+    });
   },
 };
