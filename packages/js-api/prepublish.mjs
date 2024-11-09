6f82822c46795705d63b755c286671242cd772b7
christianmat <christian_mathiesen@alumni.brown.edu>
2024-11-08 16:09:40 -0800

    fix: prepublish script for js-api

--- packages/js-api/prepublish.mjs
+++ packages/js-api/prepublish.mjs
@@ -1,16 +1,17 @@
-import {writeFileSync} from 'fs';
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
-import packageJson from './package.json' assert {type: 'json'};
+import { writeFileSync, readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 // Mimic __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // Define the path to the version file
-const versionFilePath = resolve(__dirname, 'src/version.ts');
+const versionFilePath = resolve(__dirname, 'src/version.ts')
 
 // Write the version to the file
-writeFileSync(versionFilePath, `export const SDK_VERSION = '${packageJson.version}';`, 'utf8');
+writeFileSync(versionFilePath, `export const SDK_VERSION = '${packageJson.version}';`, 'utf8')
 
 console.log(`Successfully wrote version number`)
