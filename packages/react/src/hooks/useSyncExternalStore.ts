a1a4d3d4d9177448cf1ee3c938826b648d0965ce
Micah Snyder <micah@frigade.com>
2024-12-09 12:37:37 -0800

    Fix issue with ESM import of use-sync-external-store. See: https://github.com/facebook/react/pull/25231

--- packages/react/src/hooks/useSyncExternalStore.ts
+++ packages/react/src/hooks/useSyncExternalStore.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as UseSyncExternalStoreShim from 'use-sync-external-store/shim'
+import * as UseSyncExternalStoreShim from 'use-sync-external-store/shim/index.js'
 
 // useSyncExternalStore doesn't exist in React 17, so shim it if necessary
 const useSyncExternalStore =
